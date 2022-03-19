4e139edb06f340041c8b17b248284628dfb0f2ee
juhyunham <judev0227@gmail.com>
2022-03-19 15:13:37 +0900

    상품 detail페이지 update

--- shopping-mall/src/components/products/item.tsx
+++ shopping-mall/src/components/products/item.tsx
@@ -1,21 +1,22 @@
 import { Product } from "../../types";
+import { Link } from "react-router-dom";
 
 import styled from "styled-components";
 
 const ProductItem = ({ category, description, id, image, price, rating, title }: Product) => {
   return (
     <ProdItem key={`${category}=${id}`}>
-      <ProdCategory>{category}</ProdCategory>
-      <p>{description}</p>
-      <span>{id}</span>
-      <ProdImg>
-        <source media="(min-width:650px)" srcSet={image} />
-        <source media="(min-width:465px)" srcSet={image} />
-        <img src={image} alt={description} />
-      </ProdImg>
-      <span>{price}</span>
-      <span>{rating.rate}</span>
-      <p>{title}</p>
+      <Link to={`/products/${id}`}>
+        <ProdCategory>{category}</ProdCategory>
+        <ProdTitle>{title}</ProdTitle>
+        <ProdImg>
+          <source media="(min-width:650px)" srcSet={image} />
+          <source media="(min-width:465px)" srcSet={image} />
+          <img src={image} alt={description} />
+        </ProdImg>
+        <span>가격: {price}</span>
+        <span>평점: {rating.rate}</span>
+      </Link>
     </ProdItem>
   );
 };
@@ -33,12 +34,20 @@ export const ProdItem = styled.li`
 `;
 
 export const ProdCategory = styled.h3`
-  font-size: 20px;
+  font-size: 12px;
+  font-weight: normal;
+`;
+
+export const ProdTitle = styled.h2`
+  font-size: 16px;
 `;
 
 export const ProdImg = styled.picture`
+  margin: 10px 0;
+
   img {
-    max-width: 100%;
-    max-height: 100%;
+    width: 100%;
+    height: 150px;
+    object-fit: contain;
   }
 `;
--- shopping-mall/src/pages/products/[id].tsx
+++ shopping-mall/src/pages/products/[id].tsx
@@ -1,5 +1,71 @@
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { fetcher, QueryKeys } from "../../queryClient";
+import { Product } from "../../types";
+
+import styled from "styled-components";
+
 const ProductDetail = () => {
-  return <div>상세 페이지</div>;
+  const { id } = useParams();
+
+  const { data } = useQuery<Product>([QueryKeys.PRODUCTS, id], () =>
+    fetcher({
+      method: "GET",
+      path: `/products/${id}`,
+    })
+  );
+
+  if (!data) return null;
+
+  const { category, title, image, description, price, rating } = data;
+
+  return (
+    <ProdDetailItem key={`${category}=${id}`}>
+      <ProdDetailCategory>{category}</ProdDetailCategory>
+      <ProdDetailTitle>{title}</ProdDetailTitle>
+      <ProdDetailImg>
+        <source media="(min-width:650px)" srcSet={image} />
+        <source media="(min-width:465px)" srcSet={image} />
+        <img src={image} alt={description} />
+      </ProdDetailImg>
+      <ProdDetailPrice>가격: {price}</ProdDetailPrice>
+      <ProdDetailRate>평점: {rating.rate}</ProdDetailRate>
+    </ProdDetailItem>
+  );
 };
 
 export default ProductDetail;
+
+export const ProdDetailItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  padding: 20px;
+`;
+
+export const ProdDetailCategory = styled.h3`
+  font-size: 13px;
+  font-weight: normal;
+`;
+
+export const ProdDetailTitle = styled.h2`
+  font-size: 20px;
+`;
+
+export const ProdDetailImg = styled.picture`
+  margin: 10px 0;
+
+  img {
+    width: 100%;
+    height: 400x;
+    object-fit: contain;
+  }
+`;
+
+export const ProdDetailPrice = styled.span`
+  font-size: 15px;
+`;
+
+export const ProdDetailRate = styled.span`
+  font-size: 15px;
+`;
