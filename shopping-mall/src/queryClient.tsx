bb18fcb13e82777a9d66d721095cdf09f05d4358
juhyunham <judev0227@gmail.com>
2022-03-19 15:53:20 +0900

    캐시타임 설정
    - 이전에 부른 데이터를 다시 요청안함.
    (fetch/xhr)

--- shopping-mall/src/queryClient.tsx
+++ shopping-mall/src/queryClient.tsx
@@ -6,7 +6,18 @@ type AniOBJ = { [key: string]: any };
 export const getClient = (() => {
   let client: QueryClient | null = null;
   return () => {
-    if (!client) client = new QueryClient();
+    if (!client)
+      client = new QueryClient({
+        defaultOptions: {
+          queries: {
+            cacheTime: 1000 * 60 * 60 * 24,
+            staleTime: 1000 * 60,
+            refetchOnMount: false,
+            refetchOnReconnect: false,
+            refetchOnWindowFocus: false,
+          },
+        },
+      });
     return client;
   };
 })();
